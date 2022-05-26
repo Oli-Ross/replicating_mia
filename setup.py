22e0ed936e661eea1c14f4f246aebd5c11cba8ef
Oliver Roß <mail@oliver-ross.at>
2022-05-26 14:44:20 +0200

    Generate docs from .py files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='replicating_mia',
-      version='0.1',
-      description='Replication of the membership inference attack',
-      author='Oliver Roß',
-      author_email='mail@oliver-ross.at',
-      url='https://github.com/OliverRoss/replicating_mia',
-      package_dir={'': 'src'}
-      )
