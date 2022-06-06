3e04ca21bb64873e6da2cad7956724b4572a7e98
Oli Roß <36341956+Oli-Ross@users.noreply.github.com>
2022-06-07 00:23:02 +0200

    Rename files

--- mia/configuration.py
+++ mia/configuration.py
@@ -8,7 +8,7 @@ if not os.path.isdir(configDir):
     os.mkdir(configDir)
 
 
-class ConfigFilePath():
+class ConfigFilePath:
     """
     Contains the absolute path to a configuration file.
 
@@ -45,12 +45,12 @@ class ConfigFilePath():
         located in `config/`.
         """
         now = datetime.datetime.now().strftime("%d.%m.%Y_%H:%M")
-        fileName = f"{now}.yaml"
+        fileName = f"{now}.yml"
         configFilePath = os.path.join(configDir, fileName)
         return cls(configFilePath)
 
 
-class Configuration():
+class Configuration:
     """
     Holds configuration data.
 
@@ -60,6 +60,7 @@ class Configuration():
     """
 
     def __init__(self) -> None:
+        print("t")
         pass
 
     def save_to_file(self, configName: str | None = None):
@@ -85,6 +86,7 @@ class Configuration():
         Return a Configuration object loaded from a given file path.
         """
         absoluteFilePath = configFilePath.absoluteFilePath
+        print(absoluteFilePath)
         assert os.path.isfile(absoluteFilePath)
         with open(absoluteFilePath) as yamlFile:
             return yaml.load(yamlFile, Loader=yaml.Loader)
--- mia/test/test_config.py
+++ mia/test/test_config.py
@@ -1,23 +1,23 @@
-import config
+import configuration
 import os.path
 
 
 class TestConfigFilePath():
     def test_from_rel_path(self):
-        filePath = "../config/test.yaml"
-        con = config.ConfigFilePath.from_rel_path(filePath)
+        filePath = "../config/test.yml"
+        con = configuration.ConfigFilePath.from_rel_path(filePath)
         actualPath = os.path.abspath(con.absoluteFilePath)
         expectedPath = os.path.abspath(
             os.path.join(
-                config.configDir,
-                "test.yaml"))
+                configuration.configDir,
+                "test.yml"))
 
         assert expectedPath == actualPath
 
 
 class TestConfiguration():
     def test_from_name(self):
-        con = config.Configuration.from_name("example.yaml")
+        con = configuration.Configuration.from_name("example.yml")
 
         assert con.seed["numpy"] == 1234
         assert con.name == "Example Configuration"
