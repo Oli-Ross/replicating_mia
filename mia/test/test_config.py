7e9684442a96bcb99da5d618a62ac4626bbabf4e
Oli Roß <36341956+Oli-Ross@users.noreply.github.com>
2022-06-07 00:51:30 +0200

    Restructure yml

--- mia/test/test_config.py
+++ mia/test/test_config.py
@@ -19,5 +19,5 @@ class TestConfiguration():
     def test_from_name(self):
         con = configuration.Configuration.from_name("example.yml")
 
-        assert con.seed["numpy"] == 1234
+        assert con.seed == 1234
         assert con.name == "Example Configuration"
