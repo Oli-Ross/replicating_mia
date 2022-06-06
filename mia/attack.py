1a01a55c04e8479bd75a02be55cdabcc5032ea23
Oliver Roß <mail@oliver-ross.at>
2022-06-07 01:03:51 +0200

    Add stub for starting MIA attack

--- mia/attack.py
+++ mia/attack.py
@@ -1,5 +1,32 @@
+import argparse
+from typing import Dict
+
 import datasets
+from configuration import ConfigFilePath, Configuration
+
+
+def parse_args() -> Dict:
+    parser = argparse.ArgumentParser(
+        description='Launch a membership inference attack')
+    parser.add_argument(
+        '--config',
+        help='Path to config file.',
+    )
+
+    return vars(parser.parse_args())
+
+
+def set_seeds(config: Configuration):
+    datasets.set_seed(config.seed)
+
 
 if __name__ == "__main__":
-    dataset = datasets.KagglePurchaseDataset()
-    dataset2 = datasets.Cifar10Dataset()
+    options = parse_args()
+    try:
+        config = Configuration.from_rel_path(options["config"])
+        print("Using provided configuration.")
+    except BaseException:
+        config = Configuration.from_name("example.yml")
+        print("Using default configuration.")
+
+    set_seeds(config)
