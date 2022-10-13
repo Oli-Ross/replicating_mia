b18e64ce9a5f6a7ed4595adb0d7f240348337e79
Oli Roß <36341956+Oli-Ross@users.noreply.github.com>
2022-10-13 12:07:04 +0200

    Sort attack data labels by ground truth
    And clean up code

--- mia/attack_data.py
+++ mia/attack_data.py
@@ -14,53 +14,65 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.utils import to_categorical
 
 
+def _prepare_subset(superset: Dataset, size: int,
+                    model: Sequential, inTraining: bool):
+    # TODO: hardcoded batch size
+    batchSize: int = 1
+    subset = superset.take(size).batch(batchSize)
+    predictions = model.predict(subset, batch_size=batchSize)
+    if inTraining:
+        labels = np.ones(size)
+    else:
+        labels = np.zeros(size)
+    return predictions, labels
+
+
+def _get_filter_fn(label: int):
+
+    wantedLabel = np.int64(label)
+    def _filter_fn(x, y): return tf.math.equal(wantedLabel, tf.math.argmax(y))
+    return _filter_fn
+
+
 def from_target_data(targetTrainData: Dataset,
                      targetTestData: Dataset, targetModel: Sequential) -> Dataset:
     # TODO: don't hardcode dataset size
     # TODO assertions about disjoint sets, and equal set sizes
     # TODO: Make this understandable without my piece of paper
+    label = 2
+    targetTrainData = targetTrainData.filter(_get_filter_fn(label))
+    targetTestData = targetTestData.filter(_get_filter_fn(label))
 
-    attackTrainSize: int = 10000
-    attackTestSize: int = 2000
+    # There are only limited data points per class, thus we use as many as we
+    # can get and split them 80/20 for training
+    dataSizePerSet: int = min(len(list(targetTrainData.as_numpy_iterator())), len(
+        list(targetTestData.as_numpy_iterator())))
+
+    splitFactor = 0.8
+    attackTestSize = int((1 - splitFactor) * dataSizePerSet)
+    attackTrainSize = int(splitFactor * dataSizePerSet)
+
+    print(f"Train dataset size (train) for label {label}: {attackTrainSize}")
+
     halfAttackTrainSize: int = int(attackTrainSize / 2)
     halfAttackTestSize: int = int(attackTestSize / 2)
 
-    A = targetTestData.take(halfAttackTrainSize).batch(100)
-    B = targetTrainData.take(halfAttackTrainSize).batch(100)
-    C = targetTrainData.skip(halfAttackTrainSize).take(
-        halfAttackTestSize).batch(100)
-    D = targetTestData.skip(halfAttackTrainSize).take(
-        halfAttackTestSize).batch(100)
+    APredictions, ALabels = _prepare_subset(
+        targetTestData, halfAttackTrainSize, targetModel, False)
+    BPredictions, BLabels = _prepare_subset(
+        targetTrainData, halfAttackTrainSize, targetModel, True)
+    CPredictions, CLabels = _prepare_subset(
+        targetTrainData.skip(halfAttackTrainSize), halfAttackTestSize, targetModel, True)
+    DPredictions, DLabels = _prepare_subset(
+        targetTestData.skip(halfAttackTrainSize), halfAttackTestSize, targetModel, False)
 
-    APredictions = targetModel.predict(A, batch_size=100)
-    BPredictions = targetModel.predict(B, batch_size=100)
-    CPredictions = targetModel.predict(C, batch_size=100)
-    DPredictions = targetModel.predict(D, batch_size=100)
+    featuresTrain = np.append(APredictions, BPredictions, axis=0)
+    featuresTest = np.append(CPredictions, DPredictions, axis=0)
+    labelsTrain = to_categorical(np.append(ALabels, BLabels, axis=0))
+    labelsTest = to_categorical(np.append(CLabels, DLabels, axis=0))
 
-    ALabels = np.zeros(halfAttackTrainSize)
-    BLabels = np.ones(halfAttackTrainSize)
-    CLabels = np.ones(halfAttackTestSize)
-    DLabels = np.zeros(halfAttackTestSize)
-
-    featuresTrain = np.append(
-        APredictions,
-        BPredictions,
-        axis=0).astype(
-        np.float64)
-    featuresTest = np.append(
-        CPredictions,
-        DPredictions,
-        axis=0).astype(
-        np.float64)
-
-    labelsTrain = np.append(ALabels, BLabels, axis=0).astype(np.int32)
-    labelsTest = np.append(CLabels, DLabels, axis=0).astype(np.int32)
-    labelsTrain = to_categorical(labelsTrain)
-    labelsTest = to_categorical(labelsTest)
-    attackTrainData = Dataset.from_tensor_slices(
-        (featuresTrain, labelsTrain))
-    attackTestData = Dataset.from_tensor_slices(
-        (featuresTest, labelsTest))
+    attackTrainData = Dataset.from_tensor_slices((featuresTrain, labelsTrain))
+    attackTestData = Dataset.from_tensor_slices((featuresTest, labelsTest))
 
     # TODO: sort the data by its ground truth and only return partitions
 
