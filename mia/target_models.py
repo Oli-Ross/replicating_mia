79e87e0582c838f65f82bfec3e49fd26a97c0665
Oliver Roß <mail@oliver-ross.at>
2022-11-02 14:29:51 +0100

    Use hyperpars for target model name during training

--- mia/target_models.py
+++ mia/target_models.py
@@ -99,7 +99,7 @@ def save_model(name: str, model: Sequential) -> None:
     model.save(filePath)
 
 
-def train_model(model: Sequential, trainData: Dataset,
+def train_model(model: Sequential, modelName: str, trainData: Dataset,
                 testData: Dataset, hyperpar: Dict):
     epochs: int = int(hyperpar["epochs"])
     learningRate: float = float(hyperpar["learningRate"])
@@ -112,7 +112,7 @@ def train_model(model: Sequential, trainData: Dataset,
     model.compile(optimizer, loss, metrics)
     trainData = trainData.batch(batchSize, drop_remainder=True)
     testData = testData.batch(batchSize, drop_remainder=True)
-    log_dir = "logs/target/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_dir = "logs/attack/" + modelName
     cb = keras.callbacks.TensorBoard(histogram_freq=1, log_dir=log_dir)
     return model.fit(trainData, epochs=epochs,
                      callbacks=[cb], validation_data=testData)
