a6e3a51c7e629f34cf0fff772aeb82d8bb2cbbaa
Oliver Roß <mail@oliver-ross.at>
2022-11-11 11:03:41 +0100

    Add test for from_abs_path

--- mia/test/test_configuration.py
+++ mia/test/test_configuration.py
@@ -1,4 +1,5 @@
 import configuration as con
+import pytest
 import os.path
 
 
@@ -12,6 +13,14 @@ class TestConfiguration():
     def test_from_rel_path(self):
         curDir = os.path.dirname(__file__)
         configFile = os.path.join(curDir, "../../config/example.yml")
+        config = con.from_rel_path(configFile)
+
+        assert config["seed"] == 1234
+        assert config["name"] == "Example Configuration"
+
+    @pytest.mark.skip("Hardcoded path.")
+    def test_from_abs_path(self):
+        configFile = "/home/oliver/Oliver/Studium/Master_Informatik/master/code/config/example.yml"
         config = con.from_rel_path(configFile)
 
         assert config["seed"] == 1234
