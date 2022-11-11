6116b57c4b3d1c12123f01d380e3583e78ae7ddb
Oliver Roß <mail@oliver-ross.at>
2022-11-11 11:01:56 +0100

    Add test for from_rel_path

--- mia/test/test_configuration.py
+++ mia/test/test_configuration.py
@@ -1,9 +1,18 @@
 import configuration as con
+import os.path
 
 
 class TestConfiguration():
     def test_from_name(self):
         config = con.from_name("example.yml")
+
+        assert config["seed"] == 1234
+        assert config["name"] == "Example Configuration"
+
+    def test_from_rel_path(self):
+        curDir = os.path.dirname(__file__)
+        configFile = os.path.join(curDir, "../../config/example.yml")
+        config = con.from_rel_path(configFile)
 
         assert config["seed"] == 1234
         assert config["name"] == "Example Configuration"
