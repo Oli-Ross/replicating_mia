4e854a22eadd0164fb6489cdb74d8186802cfbcf
Oliver Roß <mail@oliver-ross.at>
2022-11-14 18:41:29 +0100

    Add tests for _randomize_features

--- mia/test/test_shadow_data.py
+++ mia/test/test_shadow_data.py
@@ -1,6 +1,7 @@
+from numpy.testing import assert_equal
 import shadow_data as sd
 import numpy as np
-from numpy.testing import assert_equal
+sd.set_seed(1234)
 
 
 class TestHillClimbing():
@@ -20,3 +21,19 @@ class TestHillClimbing():
         b = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3,
                      3, 4, 4, 4]).reshape((size, 1))
         assert_equal(a, b)
+
+    def test__randomize_features(self):
+        a = np.repeat(0, 10).reshape((1, 10))
+        b = sd._randomize_features(a, 2, numFeatures=10)
+
+        assert b[0, 1] == 1
+        assert b[0, 7] == 1
+
+        a = np.repeat(0, 100).reshape((1, 100))
+        b = sd._randomize_features(a, 3, numFeatures=100)
+        print(b)
+
+        assert b[0, 0] == 1
+        assert b[0, 11] == 1
+        assert b[0, 74] == 1
+        # Magic numbers in the assert statements come from seeded RNG
