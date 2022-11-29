537275c5d8012746e8ec5765fb1bba871ecf6552
Oli Roß <36341956+Oli-Ross@users.noreply.github.com>
2022-11-29 17:18:26 +0100

    Move shadow data loading/generation back into main

--- mia/main.py
+++ mia/main.py
@@ -1,25 +1,55 @@
 from os import environ
+from os.path import isabs
+import argparse
 from typing import Dict
 
 # Tensorflow C++ backend logging verbosity
 environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # NOQA
 
+
 import download
+import configuration as con
 import target_models as tm
-import datasets
-import app_parse as parse
-import app_setup as setup
+import shadow_data as sd
+import datasets as ds
 
 
 def set_seeds(seed: int):
-    datasets.set_seed(seed)
+    ds.set_seed(seed)
     tm.set_seed(seed)
+    sd.set_seed(seed)
     #  attack_model.set_seed(seed)
     #  attack_data.set_seed(seed)
-    #  shadow_data.set_seed(seed)
 
 
-def set_up_target_model(config: Dict, targetDataset):
+def parse_args() -> Dict:
+    parser = argparse.ArgumentParser(
+        description='Launch a membership inference attack pipeline')
+    parser.add_argument(
+        '--config',
+        help='Relative path to config file.',
+    )
+
+    return vars(parser.parse_args())
+
+
+def parse_config() -> Dict:
+    options = parse_args()
+    configFile = options["config"]
+    try:
+        if isabs(configFile):
+            config = con.from_abs_path(configFile)
+        else:
+            config = con.from_rel_path(configFile)
+        name = config["name"]
+        print(f"Using configuration \"{name}\"")
+    except BaseException:
+        config = con.from_name("example.yml")
+        print("Using default configuration.")
+    return config
+
+
+def get_target_model(config: Dict, targetDataset):
     dataConfig = config["targetDataset"]
     modelConfig = config["targetModel"]["hyperparameters"]
 
@@ -43,27 +73,65 @@ def set_up_target_model(config: Dict, targetDataset):
             dataConfig["testSize"])
 
         if dataConfig["shuffle"]:
-            trainData = datasets.shuffle(trainData)
+            trainData = ds.shuffle(trainData)
 
         model = tm.KaggleModel(config["targetModel"]["classes"])
 
         tm.train_model(model, modelName, trainData, testData, modelConfig)
+
+        print("Saving target model to disk.")
         tm.save_model(modelName, model)
         tm.evaluate_model(model, testData)
 
     return model
 
 
+def get_shadow_data(config: Dict, targetDataset, targetModel) -> ds.Dataset:
+    shadowConfig = config["shadowDataset"]
+    method = shadowConfig["method"]
+    dataSize = shadowConfig["size"]
+    hyperpars = shadowConfig[method]["hyperparameters"]
+
+    if method == "noisy":
+        dataName = f'{method}_fraction_{hyperpars["fraction"]}_size_{dataSize}'
+        try:
+            shadowData = ds.load_shadow(dataName)
+        except BaseException:
+            print("Loading failed, generating shadow data.")
+            shadowData = sd.generate_shadow_data_noisy(
+                targetDataset, dataSize, **hyperpars)
+            ds.save_shadow(shadowData, dataName)
+    elif method == "hill_climbing":
+        dataName = \
+            f'{method}_' + \
+            f'kmax_{hyperpars["k_max"]}_' + \
+            f'kmin_{hyperpars["k_min"]}_' + \
+            f'confmin_{hyperpars["conf_min"]}_' + \
+            f'rejmax_{hyperpars["rej_max"]}_' + \
+            f'itermax_{hyperpars["iter_max"]}_' + \
+            f'size_{dataSize}'
+        try:
+            shadowData = ds.load_shadow(dataName)
+        except BaseException:
+            print("Loading failed, generating shadow data.")
+            shadowData = sd.hill_climbing(targetModel, dataSize, **hyperpars)
+            ds.save_shadow(shadowData, dataName)
+    else:
+        raise ValueError(f"{method} is not a valid shadow data method.")
+
+    return shadowData
+
+
 def main():
 
-    config = parse.parse_config()
+    config = parse_config()
     set_seeds(config["seed"])
 
     download.download_all_datasets()
 
-    targetDataset = datasets.load_dataset(config["targetDataset"]["name"])
-    targetModel = set_up_target_model(config, targetDataset)
-    shadowDataset = setup.get_shadow_data(config, targetDataset, targetModel)
+    targetDataset = ds.load_dataset(config["targetDataset"]["name"])
+    targetModel = get_target_model(config, targetDataset)
+    shadowDataset = get_shadow_data(config, targetDataset, targetModel)
 
 
 if __name__ == "__main__":
