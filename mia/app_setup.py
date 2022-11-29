069d7393747589e3db674c85ff372f86b5f276a7
Oliver Roß <mail@oliver-ross.at>
2022-11-29 16:45:20 +0100

    Retrain target model as necessary, infer name from hyperpars

--- mia/app_setup.py
+++ mia/app_setup.py
@@ -14,50 +14,6 @@ def set_seeds(seed: int):
     shadow_data.set_seed(seed)
 
 
-def _get_target_model_name(config: Dict) -> str:
-    epochs: int = config["targetModel"]["hyperparameters"]["epochs"]
-    batchSize: int = config["targetModel"]["hyperparameters"]["batchSize"]
-    learningRate: float = config["targetModel"]["hyperparameters"]["learningRate"]
-    trainSize: int = config["targetDataset"]["trainSize"]
-    modelName: str = f"lr_{learningRate}_bs_{batchSize}_epochs_{epochs}_trainsize_{trainSize}"
-    return modelName
-
-
-def set_up_target_model(config: Dict, targetDataset):
-
-    trainSize: int = config["targetDataset"]["trainSize"]
-    testSize: int = config["targetDataset"]["testSize"]
-
-    targetTrainData = targetDataset.take(trainSize)
-    targetTestData = targetDataset.skip(trainSize).take(testSize)
-
-    if config["targetDataset"]["shuffle"]:
-        targetTrainData = datasets.shuffle(targetTrainData)
-
-    if config["actions"]["trainTarget"]:
-        targetModel: target_models.KaggleModel = target_models.KaggleModel(
-            config["targetModel"]["classes"])
-        targetModelName: str = _get_target_model_name(config)
-
-        target_models.train_model(
-            targetModel,
-            targetModelName,
-            targetTrainData,
-            targetTestData,
-            config["targetModel"]["hyperparameters"])
-        target_models.save_model(targetModelName, targetModel)
-    else:
-        targetModelName: str = config["targetModel"]["name"]
-        targetModel: target_models.KaggleModel = target_models.load_model(
-            targetModelName)
-
-    # Evaluate target model
-    if config["actions"]["testTarget"]:
-        target_models.evaluate_model(targetModel, targetTestData)
-
-    return targetModel
-
-
 def _get_attack_model_name(label: int, config: Dict) -> str:
     epochs: int = config["attackModel"]["hyperparameters"]["epochs"]
     batchSize: int = config["attackModel"]["hyperparameters"]["batchSize"]
--- mia/main.py
+++ mia/main.py
@@ -1,23 +1,68 @@
 from os import environ
+from typing import Dict
 
 # Tensorflow C++ backend logging verbosity
 environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # NOQA
 
 import download
+import target_models as tm
 import datasets
 import app_parse as parse
 import app_setup as setup
 
 
+def set_seeds(seed: int):
+    datasets.set_seed(seed)
+    tm.set_seed(seed)
+    #  attack_model.set_seed(seed)
+    #  attack_data.set_seed(seed)
+    #  shadow_data.set_seed(seed)
+
+
+def set_up_target_model(config: Dict, targetDataset):
+    dataConfig = config["targetDataset"]
+    modelConfig = config["targetModel"]["hyperparameters"]
+
+    modelName = \
+        f'{dataConfig["name"]}_' + \
+        f'lr_{modelConfig["learningRate"]}_' + \
+        f'bs_{modelConfig["batchSize"]}_' + \
+        f'epochs_{modelConfig["epochs"]}_' + \
+        f'trainsize_{dataConfig["trainSize"]}'
+
+    try:
+        print(f"Trying to load model \"{modelName}\" from disk.")
+        model: tm.KaggleModel = tm.load_model(modelName)
+
+    except BaseException:
+        print("Didn't work, retraining target model.")
+
+        trainData = targetDataset.take(dataConfig["trainSize"])
+        testData = targetDataset.skip(
+            dataConfig["trainSize"]).take(
+            dataConfig["testSize"])
+
+        if dataConfig["shuffle"]:
+            trainData = datasets.shuffle(trainData)
+
+        model = tm.KaggleModel(config["targetModel"]["classes"])
+
+        tm.train_model(model, modelName, trainData, testData, modelConfig)
+        tm.save_model(modelName, model)
+        tm.evaluate_model(model, testData)
+
+    return model
+
+
 def main():
 
     config = parse.parse_config()
-    setup.set_seeds(config["seed"])
+    set_seeds(config["seed"])
 
     download.download_all_datasets()
 
     targetDataset = datasets.load_dataset(config["targetDataset"]["name"])
-    targetModel = setup.set_up_target_model(config, targetDataset)
+    targetModel = set_up_target_model(config, targetDataset)
     shadowDataset = setup.get_shadow_data(config, targetDataset, targetModel)
 
 
