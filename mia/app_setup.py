e797dcf1f570c61661530532d413e547365c1841
Oliver Roß <mail@oliver-ross.at>
2022-11-29 17:18:26 +0100

    Move shadow data loading/generation back into main

--- mia/app_setup.py
+++ mia/app_setup.py
@@ -25,24 +25,3 @@ def _get_attack_model_name(label: int, config: Dict) -> str:
 def load_attack_model(config: Dict):
     attackModelName: str = config["attackModel"]["name"]
     return attack_model.load_model(attackModelName)
-
-
-def get_shadow_data(config: Dict, targetDataset,
-                    targetModel) -> datasets.Dataset:
-    shadowDataName: str = config["shadowDataset"]["name"]
-    if config["actions"]["generateShadowData"]:
-        method = config["shadowDataset"]["method"]
-        if method == "noisy":
-            shadowDataset: datasets.Dataset = shadow_data.generate_shadow_data_noisy(
-                targetDataset, outputSize=500000)
-        elif method == "hill_climbing":
-            shadowDataset: datasets.Dataset = shadow_data.hill_climbing(
-                targetModel,
-                config["shadowDataset"]["size"],
-                **config["shadowDataset"]["hill_climbing"]["hyperparameters"])
-        else:
-            raise ValueError(f"{method} is not a valid shadow data method.")
-        datasets.save_shadow(shadowDataset, shadowDataName)
-    else:
-        shadowDataset: datasets.Dataset = datasets.load_shadow(shadowDataName)
-    return shadowDataset
