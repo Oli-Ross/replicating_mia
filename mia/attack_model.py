a17283779c091a9109dcc1e9672620fabf415087
Oli Roß <36341956+Oli-Ross@users.noreply.github.com>
2023-04-18 22:01:14 +0200

    Fix misleading print statement

--- mia/attack_model.py
+++ mia/attack_model.py
@@ -145,7 +145,7 @@ def get_attack_models(config: Dict, attackDatasets: List[Tuple[ds.Dataset, ds.Da
             train_model(model, modelName, trainData, testData, modelConfig)
 
             if config["cache_to_disk"]:
-                print(f"Saving target model {i} to disk.")
+                print(f"Saving attack model {i} to disk.")
                 save_model(modelName, model)
             evaluate_model(model, testData)
 
