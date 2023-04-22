aa183094ba0256849e513392876dabd525873994
Oli Roß <36341956+Oli-Ross@users.noreply.github.com>
2023-04-22 11:00:17 +0200

    Move try to relevant statement only

--- mia/shadow_data.py
+++ mia/shadow_data.py
@@ -102,15 +102,15 @@ def get_shadow_data(config: Dict, targetDataset, targetModel) -> ds.Dataset:
         else:
             raise ValueError(f"{method} is not a valid shadow data method.")
 
-        try:
-            if config["cache_to_disk"]:
-                if verbose:
-                    print(f"Saving shadow data {dataName} to disk.")
+        if config["cache_to_disk"]:
+            if verbose:
+                print(f"Saving shadow data {dataName} to disk.")
+            try:
                 ds.save_shadow(shadowData, dataName)
-        except BaseException:
-            print(f"Failed to save shadow data {dataName} to disk.")
-            ds.delete_shadow(dataName)
-            raise
+            except BaseException:
+                print(f"Failed to save shadow data {dataName} to disk.")
+                ds.delete_shadow(dataName)
+                raise
 
     return shadowData
 
