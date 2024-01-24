8984f24ddb47206e376bf2dddfe98fd82dc79231
Oli Roß <36341956+Oli-Ross@users.noreply.github.com>
2024-01-24 10:07:43 +0100

    Add stub file to run attack pipeline on target model

---
+++ mia/attack_pipeline.py
@@ -0,0 +1,61 @@
+"""
+.. include:: ../docs/attack_pipeline.md
+"""
+
+from os import environ
+
+# Tensorflow C++ backend logging verbosity
+environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # NOQA
+
+from typing import Dict, Tuple
+
+from tensorflow.data import Dataset  # pyright: ignore
+from tensorflow.python.framework import random_seed
+
+import datasets as ds
+import target_models as tm
+import attack_model as am
+
+global_seed: int = 1234
+
+
+def set_seed(new_seed: int):
+    """
+    Set the global seed that will be used for all functions that include
+    randomness.
+    """
+    global global_seed
+    global_seed = new_seed
+    random_seed.set_seed(global_seed)
+
+def load_target_data(config:Dict) -> Tuple[Dataset, Dataset]:
+    targetModelName = tm.get_model_name(config)
+    targetTrainDataName = targetModelName + "_train_data"
+    targetRestDataName = targetModelName + "_rest_data"
+    targetTrainData = ds.load_target(targetTrainDataName)
+    targetRestData = ds.load_target(targetRestDataName)
+    return targetTrainData, targetRestData
+
+def run_pipeline(attackModels, targetModel, targetTrainData, targetRestData):
+    pass
+
+if __name__ == "__main__":
+    import argparse
+    import configuration as con
+    import attack_data as ad
+
+    parser = argparse.ArgumentParser(description='Save one shadow dataset per model and train the models.')
+    parser.add_argument('--config', help='Relative path to config file.',)
+    config = con.from_cli_options(vars(parser.parse_args()))
+    set_seed(config["seed"])
+
+    
+    targetDataset = ds.load_dataset(config["targetDataset"]["name"])
+    targetModel = tm.get_target_model(config, targetDataset)
+
+    targetTrainData, targetRestData = load_target_data(config)
+
+    attackDatasets = ad.load_attack_data(config)
+    attackModels = am.get_attack_models(config, attackDatasets)
+
+    run_pipeline(attackModels, targetModel, targetTrainData, targetRestData)
