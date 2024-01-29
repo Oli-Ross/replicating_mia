76547ef613a06bb954727e666e6d54c17ceff8c7
Oli Roß <36341956+Oli-Ross@users.noreply.github.com>
2024-01-29 14:16:10 +0100

    Use shadow data generation in model names.
    To detect wether retraining is necessary

--- mia/shadow_models.py
+++ mia/shadow_models.py
@@ -23,8 +23,9 @@ def set_seed(new_seed: int):
 
 def get_shadow_model_name(config: Dict, i: int):
     numModels: int = config["shadowModels"]["number"]
+    method: str = config["shadowDataset"]["method"]
     split: float = config["shadowModels"]["split"]
-    return "shadow_" + tm.get_model_name(config) + f"_split_{split}_{i+1}_of_{numModels}"
+    return "shadow_" + method + tm.get_model_name(config) + f"_split_{split}_{i+1}_of_{numModels}"
 
 
 def load_shadow_models_and_datasets(config: Dict) -> Tuple[List[tm.Sequential], List[Tuple[ds.Dataset, ds.Dataset]]]:
